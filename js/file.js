a3f30d2f8faa2e238abb0c4b8d10cefac54f1cbb
Bijoy Chandro Das <freelancing.bijoy@gmail.com>
2022-09-02 13:37:54 +0600

    menu added

--- js/file.js
+++ js/file.js
@@ -1,1 +1,26 @@
-console.log('added js file')
+const loadAllProducts = async () => {
+    const url = `https://fakestoreapi.com/products`;
+    const res = await fetch(url);
+    const data = await res.json();
+    return data;
+}
+
+const setAllmenu = async () => {
+    const data = await loadAllProducts();
+    // console.log(data)
+
+    const menu = document.getElementById('all-menu');
+    for (const product of data) {
+        // console.log(product.category)
+        const li = document.createElement('li');
+        li.innerHTML = `
+        <a>${product.category}</a>
+      `
+        menu.appendChild(li)
+
+
+    }
+
+}
+
+setAllmenu();
